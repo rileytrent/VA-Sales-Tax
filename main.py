5622d0a5a68f5a00bd2ce6ac8338e0cec3899097
rileytrent <rileytrent8@gmail.com>
2025-06-09 21:12:06 -0700

    Open the return for last month

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from dateutil.relativedelta import relativedelta
+import datetime
+
 
 
 load_dotenv()
@@ -20,7 +23,15 @@ FILING_URL = os.getenv("FILING_URL")
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
-#Opens Page and dismisses weird warning.
+def get_current_filing_month():
+    last_month = datetime.datetime.now() - relativedelta(months=1)
+    text = last_month.strftime('%B %Y')
+    return (text)
+
+target_month_year = get_current_filing_month()
+# print(target_month_year)
+# print(f"//td[normalize-space()='{target_month_year}']/parent::tr//a[contains(text(),'File Now')]")
+# #Opens Page and dismisses weird warning.
 driver.get(FILING_URL)
 
 time.sleep(2)
@@ -28,10 +39,51 @@ time.sleep(2)
 dismiss_warning = driver.find_element(By.CSS_SELECTOR, '.btn.btn-outline-warning.waves-effect.button-submit')
 dismiss_warning.click()
 
+Tax_button = driver.find_element(By.CSS_SELECTOR, 'input[name="loginForm:customerType"][value="T"]')
+Tax_button.click()
+
+account_number = driver.find_element(By.ID, 'loginForm:customerNumber')
+account_number.send_keys(FILING_NUMBER)
+
+login = driver.find_element(By.ID, 'loginForm:userName')
+login.send_keys(USERNAME_LOGIN)
+
+password = driver.find_element(By.ID, 'loginForm:password')
+password.send_keys(PASSWORD)
+
+time.sleep(1)
+
+login_button = driver.find_element(By.NAME, 'loginForm:loginButton')
+login_button.click()
+
+time.sleep(1)
+
+file_and_pay_button = driver.find_element(By.LINK_TEXT, "File / Pay Use Tax")
+file_and_pay_button.click()
+
+time.sleep(1)
+
+iframe = driver.switch_to.frame("tapsIFrame")
+
+time.sleep(1)
+
+current_month_button = driver.find_element(By.XPATH, f"//td[normalize-space()='{target_month_year}']/parent::tr//a[contains(text(),'File Now')]")
+current_month_button.click()
 
 
+time.sleep(1)
+
+select_all_button = driver.find_element(By.LINK_TEXT, 'Select All')
+login_button.click()
+
+time.sleep(1)
+
+select_all_button = driver.find_element(By.LINK_TEXT, 'Next')
+login_button.click()
 
 
+# <input id="loginForm:loginButton" type="submit" name="loginForm:loginButton" value="Log In" class="button-submit" onclick="return buttonClick(this);" data-di-id="#loginForm:loginButton">
 
 #Log Into VA sales Tax 
 
+# f"//td[text()='{target_month_year}']/parent::tr//a[contains(text(),'File Now')]"
