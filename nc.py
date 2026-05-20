cb16c4d17cfadfc8d108d3500f45fe7d70c53991
Riley Trent <riley@Mac.localdomain>
2026-05-20 15:11:02 -0400

    reqs and nc tax fix

--- nc.py
+++ nc.py
@@ -9,39 +9,54 @@ from datetime import datetime
 import calendar
 import pandas as pd
 import subprocess
-import os 
+import os
 from dotenv import load_dotenv
 
 
-
-
-#Local Dev testing
+# Local Dev testing
 load_dotenv()
-nc_test_email = os.getenv("NC_SALES_TAX_CONTACT_EMAIL") #this number wont change in prod so no need to collect it in the gui
-nc_test_login_name = os.getenv("NC_SALES_TAX_CONTACT_NAME") #this number wont change in prod so no need to collect it in the gui
-nc_test_phone = os.getenv("NC_SALES_TAX_CONTACT_PHONE") #this number wont change in prod so no need to collect it in the gui
-nc_test_filing_url = os.getenv("NC_SALES_TAX_FILING_URL") #this number wont change in prod so no need to collect it in the gui
-nc_test_account_id = os.getenv("NC_SALES_TAX_ACCOUNT_ID") #this number wont change in prod so no need to collect it in the gui
+nc_test_email = os.getenv(
+    "NC_SALES_TAX_CONTACT_EMAIL"
+)  # this number wont change in prod so no need to collect it in the gui
+nc_test_login_name = os.getenv(
+    "NC_SALES_TAX_CONTACT_NAME"
+)  # this number wont change in prod so no need to collect it in the gui
+nc_test_phone = os.getenv(
+    "NC_SALES_TAX_CONTACT_PHONE"
+)  # this number wont change in prod so no need to collect it in the gui
+nc_test_filing_url = os.getenv(
+    "NC_SALES_TAX_FILING_URL"
+)  # this number wont change in prod so no need to collect it in the gui
+nc_test_account_id = os.getenv(
+    "NC_SALES_TAX_ACCOUNT_ID"
+)  # this number wont change in prod so no need to collect it in the gui
 nc_test_ein = os.getenv("BARKINGLABS_EIN")
-nc_test_period = "Oct 2025"
-nc_test_template = "NC_TAX_TEMPLATE.csv"
+nc_test_period = "Apr 2026"
+nc_test_template = "/Users/riley/Downloads/NC_TAX_TEMPLATE_2026.04.csv"
 
 
-
-#get current chrome version. 
+# get current chrome version.
 def set_value_js_by_name(driver, name, value_str):
     elem = driver.find_element(By.NAME, name)
-    driver.execute_script("""
+    driver.execute_script(
+        """
         arguments[0].value = arguments[1];
         arguments[0].defaultValue = arguments[1];  // so their code treats it as the original
-    """, elem, value_str)
+    """,
+        elem,
+        value_str,
+    )
+
 
 def get_chrome_ver():
     output = subprocess.check_output(
         ["/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", "--version"]
     )
-    return (output.decode('utf-8').strip().split()[-1])
+    return output.decode("utf-8").strip().split()[-1]
+
+
 get_chrome_ver()
+
 
 def get_filing_date(date):
     dt = datetime.strptime(date, "%b %Y")
@@ -51,7 +66,9 @@ def get_filing_date(date):
 
 
 # period,tax_id, username_login, password_entry,file_path
-def NC_file_tax(contact_name, contact_email, contact_phone, account_id, period, ein, file_path):
+def NC_file_tax(
+    contact_name, contact_email, contact_phone, account_id, period, ein, file_path
+):
     CONTACT_NAME = contact_name
     CONTACT_EMAIL = contact_email
     CONTACT_PHONE = contact_phone
@@ -60,7 +77,7 @@ def NC_file_tax(contact_name, contact_email, contact_phone, account_id, period, 
     PERIOD_INFO = PERIOD.split("-")
     MONTH = PERIOD_INFO[0]
     DAY = PERIOD_INFO[1]
-    YEAR= PERIOD_INFO[2]
+    YEAR = PERIOD_INFO[2]
     EIN = ein
     FILING_URL = "https://eservices.dor.nc.gov/sau/contact.jsp"
 
@@ -69,7 +86,7 @@ def NC_file_tax(contact_name, contact_email, contact_phone, account_id, period, 
 
     # os.getenv("FILING_URL")
 
-# service = Service(ChromeDriverManager().install())
+    # service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
 
@@ -83,129 +100,139 @@ def NC_file_tax(contact_name, contact_email, contact_phone, account_id, period, 
     driver.get(FILING_URL)
 
     time.sleep(1)
-    #Fills out Login Info
-    name = driver.find_element(By.NAME, 'name')
+    # Fills out Login Info
+    name = driver.find_element(By.NAME, "name")
     name.send_keys(CONTACT_NAME)
 
-    email = driver.find_element(By.NAME, 'email')
+    email = driver.find_element(By.NAME, "email")
     email.send_keys(CONTACT_EMAIL)
 
-    phone_number = driver.find_element(By.NAME, 'phone')
+    phone_number = driver.find_element(By.NAME, "phone")
     phone_number.send_keys(CONTACT_PHONE)
 
     time.sleep(1)
 
-    next_button = driver.find_element(By.NAME, 'img_next')
+    next_button = driver.find_element(By.NAME, "img_next")
     next_button.click()
 
     time.sleep(1)
 
-    id = driver.find_element(By.NAME, 'salesacctnumber')
+    id = driver.find_element(By.NAME, "salesacctnumber")
     id.send_keys(ACCOUNT_ID)
-    
+
     time.sleep(1)
 
-    submit_button = driver.find_element(By.NAME, 'img_submit')
+    submit_button = driver.find_element(By.NAME, "img_submit")
     submit_button.click()
 
     time.sleep(1)
 
-    next_button = driver.find_element(By.NAME, 'img_next')
+    next_button = driver.find_element(By.NAME, "img_next")
     next_button.click()
 
     time.sleep(1)
 
-    file_type_selector = driver.find_element(By.CSS_SELECTOR, "input[name='filingOption'][value='filePay']")
+    file_type_selector = driver.find_element(
+        By.CSS_SELECTOR, "input[name='filingOption'][value='filePay']"
+    )
     file_type_selector.click()
 
     time.sleep(1)
 
-    next_button = driver.find_element(By.NAME, 'img_next')
+    next_button = driver.find_element(By.NAME, "img_next")
     next_button.click()
 
     time.sleep(1)
 
-    month_selector = Select(driver.find_element(By.NAME, 'periodEndMonth'))
+    month_selector = Select(driver.find_element(By.NAME, "periodEndMonth"))
     month_selector.select_by_value(f"{MONTH}")
 
     time.sleep(1)
 
-    day_selector = Select(driver.find_element(By.NAME, 'periodEndDay'))
+    day_selector = Select(driver.find_element(By.NAME, "periodEndDay"))
     day_selector.select_by_value(f"{DAY}")
 
     time.sleep(1)
 
-    year_selector = Select(driver.find_element(By.NAME, 'periodEndYear'))
+    year_selector = Select(driver.find_element(By.NAME, "periodEndYear"))
     year_selector.select_by_value(f"{YEAR}")
 
     time.sleep(1)
 
-    fein_type_selector = driver.find_element(By.CSS_SELECTOR, "input[name='entityType'][value='001']")
+    fein_type_selector = driver.find_element(
+        By.CSS_SELECTOR, "input[name='entityType'][value='001']"
+    )
     fein_type_selector.click()
 
     time.sleep(1)
 
-    id = driver.find_element(By.NAME, 'entityIDFID1')
+    id = driver.find_element(By.NAME, "entityIDFID1")
     id.send_keys(EIN[:2])
 
-    id = driver.find_element(By.NAME, 'entityIDFID2')
+    id = driver.find_element(By.NAME, "entityIDFID2")
     id.send_keys(EIN[-7:])
 
     time.sleep(1)
 
-    next_button = driver.find_element(By.NAME, 'img_next')
+    next_button = driver.find_element(By.NAME, "img_next")
     next_button.click()
 
     time.sleep(1)
-    #Summary main page loop
+    # Summary main page loop
     for _, row in tax_data.iterrows():
-        county_juris = str(row['county_juris']) 
-        state_tax = float(row['state_tax']) 
-        high_local_tax = float(row['high_local_tax'])
-        low_local_tax = float(row['low_local_tax'])
-        transit_tax = float(row['transit_tax'])
-        gross_sales = float(row['gross_sales'])
+        county_juris = str(row["county_juris"])
+        state_tax = float(row["state_tax"])
+        high_local_tax = float(row["high_local_tax"])
+        low_local_tax = float(row["low_local_tax"])
+        transit_tax = float(row["transit_tax"])
+        gross_sales = float(row["gross_sales"])
 
         try:
-        # Find input index
-            if county_juris == 'GROSS TOTAL':
-                set_value_js_by_name(driver, 'gross-receipts', f"{gross_sales:.2f}")
-            elif county_juris == 'SUBTOTAL':
-                set_value_js_by_name(driver, 'receipts-state-0475', f"{state_tax:.2f}")
-                set_value_js_by_name(driver, 'receipts-county-020', f'{low_local_tax}')
-                set_value_js_by_name(driver, 'receipts-county-0225',f'{high_local_tax}')
-                set_value_js_by_name(driver, 'receipts-county-005', f'{transit_tax}')
+            # Find input index
+            if county_juris == "GROSS TOTAL":
+                set_value_js_by_name(driver, "gross-receipts", f"{gross_sales:.2f}")
+            elif county_juris == "SUBTOTAL":
+                set_value_js_by_name(driver, "receipts-state-0475", f"{state_tax:.2f}")
+                set_value_js_by_name(driver, "receipts-county-020", f"{low_local_tax}")
+                set_value_js_by_name(
+                    driver, "receipts-county-0225", f"{high_local_tax}"
+                )
+                set_value_js_by_name(driver, "receipts-county-005", f"{transit_tax}")
             else:
                 pass
         except Exception as e:
             print(f"Failed for row {county_juris}: {e}")
-    
+
     time.sleep(1)
 
-    next_button = driver.find_element(By.NAME, 'img_next')
+    next_button = driver.find_element(By.NAME, "img_next")
     next_button.click()
 
     time.sleep(1)
 
-    next_button = driver.find_element(By.NAME, 'img_next')
+    next_button = driver.find_element(By.NAME, "img_e536")
     next_button.click()
 
     time.sleep(1)
 
     for _, row in tax_data.iterrows():
-        county_juris = str(row['county_juris']) 
-        state_tax = float(row['state_tax']) 
-        high_local_tax = float(row['high_local_tax'])
-        low_local_tax = float(row['low_local_tax'])
-        transit_tax = float(row['transit_tax'])
-        high_local_box = str(row['county_2_25pct_input'])
-        low_local_box = str(row['county_2pct_input'])
-        transit_box = str(row['transit_0_5pct_input'])
-        gross_sales = float(row['gross_sales'])
+        county_juris = str(row["county_juris"])
+        state_tax = float(row["state_tax"])
+        high_local_tax = float(row["high_local_tax"])
+        low_local_tax = float(row["low_local_tax"])
+        transit_tax = float(row["transit_tax"])
+        high_local_box = str(row["county_2_25pct_input"])
+        low_local_box = str(row["county_2pct_input"])
+        transit_box = str(row["transit_0_5pct_input"])
+        gross_sales = float(row["gross_sales"])
 
         try:
-        # Find input index
-            if county_juris == 'GROSS TOTAL' or county_juris == 'NON TAXABLE TOTAL' or county_juris == 'GROSS TOTAL' :
+            # Find input index
+            if (
+                county_juris == "GROSS TOTAL"
+                or county_juris == "NON TAXABLE TOTAL"
+                or county_juris == "GROSS TOTAL"
+            ):
                 pass
             elif high_local_tax != 0 and low_local_tax == 0 and transit_tax == 0:
                 set_value_js_by_name(driver, high_local_box, f"{high_local_tax:.2f}")
@@ -231,8 +258,17 @@ def NC_file_tax(contact_name, contact_email, contact_phone, account_id, period, 
     totalPointFivePercent();
     """)
     time.sleep(1)
-    summary_button = driver.find_element(By.NAME, 'img_e500')
+    summary_button = driver.find_element(By.NAME, "img_e500")
     summary_button.click()
 
-#local testing 
-# NC_file_tax(nc_test_login_name, nc_test_email, nc_test_phone, nc_test_account_id, nc_test_period, nc_test_ein, nc_test_template)
+
+# local testing
+NC_file_tax(
+    nc_test_login_name,
+    nc_test_email,
+    nc_test_phone,
+    nc_test_account_id,
+    nc_test_period,
+    nc_test_ein,
+    nc_test_template,
+)
